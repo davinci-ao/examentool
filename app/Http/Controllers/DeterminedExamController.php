586abc3efbf0a5e15ed904f9539adb45101a91a2
Kevin van Sonsbeek <99041317@mydavinci.nl>
2018-10-03 12:01:20 +0200

    Created (filtered/trimmed) getAll function

--- app/Http/Controllers/DeterminedExamController.php
+++ app/Http/Controllers/DeterminedExamController.php
@@ -22,9 +22,25 @@ class DeterminedExamController extends Controller
 
    }
 
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     */
    public function getAllTrimmed()
    {
-        
+        //if db request is successfull
+        if($data = DeterminedExam::select('_id', 'exam_title', 'exam_description', 'exam_cohort')->get()) {
+            //If db request returned 0 lines
+            if($data->count() == 0) {
+                //return 404
+                return response()->json(array(), 404);
+            } else {
+                //return data, 200
+                return response()->json($data, 200);
+            }
+        } else {
+            //return 500
+            return response()->json(array(), 500);
+        }
    }
 
    public function getById($exam_id)

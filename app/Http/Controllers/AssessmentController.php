d4fb9cbabf96e7b7a47c6913ce8fee39b95fc226
Kevin van Sonsbeek <99041317@mydavinci.nl>
2018-10-05 09:51:14 +0200

    Created the models and controller for the assessment process

---
+++ app/Http/Controllers/AssessmentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Assessment;
+use App\FinalAssessment;
+use http\Env\Request;
+
+class AssessmentController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+}

f9b0d5be21f7de7696533b95574434c7ac634f95
Kevin van Sonsbeek <99041317@mydavinci.nl>
2018-10-30 13:39:16 +0100

    Update feature for Determined Exam

--- app/Http/Controllers/DeterminedExamController.php
+++ app/Http/Controllers/DeterminedExamController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\DeterminedExam;
+use Illuminate\Http\Request;
 
 class DeterminedExamController extends Controller
 {
@@ -74,11 +75,49 @@ class DeterminedExamController extends Controller
      * @param $determined_exam_id
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
      */
     public function update($determined_exam_id, Request $request)
     {
         if($determined_exam = DeterminedExam::find($determined_exam_id)) {
-            return response()->json($determined_exam, 200);
+
+            //Validate request data
+            $request_data = $this->validate($request, [
+                'exam_title' => '',
+                'exam_description' => '',
+                'exam_cohort' => '',
+                'exam_rating_algorithms' => '',
+                'exam_criteria' => ''
+            ]);
+
+            //Isset exam_title, update
+            if(isset($request_data['exam_title']))
+                $determined_exam->exam_title = $request_data['exam_title'];
+
+            //Isset exam_description, update
+            if(isset($request_data['exam_description']))
+                $determined_exam->exam_description = $request_data['exam_description'];
+
+            //Isset exam_cohort, update
+            if(isset($request_data['exam_cohort']))
+                $determined_exam->exam_cohort = $request_data['exam_cohort'];
+
+            //Isset exam_rating_algorithms, update
+            if(isset($request_data['exam_rating_algorithms']))
+                $determined_exam->exam_rating_algorithms = $request_data['exam_rating_algorithms'];
+
+            //Isset exam_criteria, update
+            if(isset($request_data['exam_criteria']))
+                $determined_exam->exam_criteria = $request_data['exam_criteria'];
+
+            //Save
+            if($determined_exam->save()) {
+                //Success, Return 200
+                return response()->json($determined_exam, 200);
+            } else {
+                //Fail, return 505
+                return response()->json($determined_exam, 505);
+            }
         } else {
             //Return 404
             return response()->json(new \stdClass(), 404);
